798de44ef91b309fe3c778ab77c74e217506a648
Ariana Garcia <arigarcia.978@gmail.com>
2016-04-19 11:44:08 -0300

    Editando routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,9 @@ Rails.application.routes.draw do
       end
    end
 
-   controller :sessions do
-      get 'session' => :checkIfUserSignedIn
-   end
+   #controller :sessions do
+   #   get 'session' => :checkIfUserSignedIn
+   #end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
