4cf7515862b2b3d6b007eac6f8424fde22eae796
Ariana Garcia <arigarcia.978@gmail.com>
2016-04-19 11:34:23 -0300

    Editando session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < ApplicationController
 
-	def checkIfUserSignedIn
-		@user = current_user
-		respond_to do |format|
-        	format.html { render :nothing => true, :notice => 'Creation SUCCESSFUL!' }
-        	format.js { render nothing: true } 
-        end
-	end
+	#def checkIfUserSignedIn
+		#@user = current_user
+		#respond_to do |format|
+        	#format.html { render :nothing => true, :notice => 'Creation SUCCESSFUL!' }
+        	#format.js { render nothing: true } 
+        #end
+	#end
 	
 end
